ae9b50587691384e0a0f3508a6f55e4ed491ce17
Luis2112Angel <luis9925.la@gmail.com>
2019-10-22 16:15:30 -0500

    update

--- geocodificacionInversa.py
+++ geocodificacionInversa.py
@@ -1,6 +1,1 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.reverse("18.847981, -97.099029")
-dict = location.raw
-return dict
+print("Hola mundo")
