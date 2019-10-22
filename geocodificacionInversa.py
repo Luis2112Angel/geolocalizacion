81571b9bba7eca805ccc069983a556e678045318
Luis2112Angel <luis9925.la@gmail.com>
2019-10-22 14:11:37 -0500

    add

--- geocodificacionInversa.py
+++ geocodificacionInversa.py
@@ -1,5 +1,12 @@
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-location = geolocator.reverse("18.847981, -97.099029")
-dict = location.raw
-print(dict)
+
+app = Flask(__name__)
+@app.route('/')
+def geo():
+    geolocator = Nominatim(user_agent="specify_your_app_name_here")
+    location = geolocator.reverse("18.847981, -97.099029")
+    dict = location.raw
+    return dict
+    
+if __name__ == '__main__':
+    app.run()
