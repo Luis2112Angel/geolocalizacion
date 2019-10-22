e10a5328f62f0f47ae7994c6c15b569147a30b4e
Luis2112Angel <luis9925.la@gmail.com>
2019-10-22 14:28:54 -0500

    add

--- geocodificacionInversa.py
+++ geocodificacionInversa.py
@@ -1,7 +1,9 @@
 from geopy.geocoders import Nominatim
 
-@app.route('/')
+def geo():
     geolocator = Nominatim(user_agent="specify_your_app_name_here")
     location = geolocator.reverse("18.847981, -97.099029")
     dict = location.raw
-    print (dict)
+    print(dict)
+
+geo()
