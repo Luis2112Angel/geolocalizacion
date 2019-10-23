280d182bbd8c5c37fa63b3550f522fef2b48372e
Luis2112Angel <luis9925.la@gmail.com>
2019-10-22 23:50:49 -0500

    update

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 def geo():
     geolocator = Nominatim(user_agent="specify_your_app_name_here")
     location = geolocator.reverse("18.847981, -97.099029")
-    dict = location.raw
-    print (dict)
+    return location.address
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
