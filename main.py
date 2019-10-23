81dd781ee639a583f51fc15d46a251428bf7efc3
Luis2112Angel <luis9925.la@gmail.com>
2019-10-22 23:42:02 -0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from geopy.geocoders import Nominatim
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def geo():
+    geolocator = Nominatim(user_agent="specify_your_app_name_here")
+    location = geolocator.reverse("18.847981, -97.099029")
+    dict = location.raw
+    return dict
