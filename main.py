e744bd6e2420cd01fee125223f891bd24b46730e
Luis2112Angel <luis9925.la@gmail.com>
2019-10-23 09:43:01 -0500

    update

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def geo():
     geolocator = Nominatim(user_agent="specify_your_app_name_here")
     location = geolocator.reverse("18.847981, -97.099029")
     dict = location.raw
-    return jsonfy({dict})
+    return jsonify(dict)
 
 app.run()
